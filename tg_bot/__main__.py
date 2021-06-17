46256295387ea0964158f7a2537f02843b889437
PattPatrich <82248145+PattPatrich@users.noreply.github.com>
2021-06-17 09:04:01 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -18,7 +18,9 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-hoi {}, my name is {}! if you have any questions about how to use me please give me /help... 
+hoi {}, my name is {}! 
+
+... 
 
 im a group manager bot for @IsItIsAsItIs.
 
